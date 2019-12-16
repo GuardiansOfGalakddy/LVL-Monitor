bf4492b7928e10b3408b793e0822c5bb9ad17cd5
JunHwaPark <a28165415@gmail.com>
2019-12-16 16:15:21 +0900

    Initial commit

---
+++ app/src/main/java/com/guardiansofgalakddy/lvlmonitor/CollectorActivity.java
@@ -0,0 +1,14 @@
+package com.guardiansofgalakddy.lvlmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collector);
+    }
+}
---
+++ app/src/main/java/com/guardiansofgalakddy/lvlmonitor/MainActivity.java
@@ -0,0 +1,76 @@
+package com.guardiansofgalakddy.lvlmonitor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_foreground);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_item_nomedia:
+                        Toast.makeText(getApplicationContext(), "nomedia", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.navigation_item_test:
+                        Toast.makeText(getApplicationContext(), "Test mic", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MonitorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CollectorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
