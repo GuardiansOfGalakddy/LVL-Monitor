eb7f58d0ec93e7a2274af7e19991729af323b449
woojin <ujini1129@gmail.com>
2019-12-17 23:34:34 +0900

    Google Map 추가
    + Google Fragment 추가
    + 시작시 내 위치로 카메라 이동
    + Google API 키는 OneGold 사용
    + Manifest 수정: Google Map 관련
    + build.gradle: AutoPermission, google service 추가
    + MainActivity: AutoPermission 설정 추가

--- app/src/main/java/com/guardiansofgalakddy/lvlmonitor/MainActivity.java
+++ app/src/main/java/com/guardiansofgalakddy/lvlmonitor/MainActivity.java
@@ -6,29 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.google.android.material.navigation.NavigationView;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
 
-public class MainActivity extends AppCompatActivity {
-    /* Map 관련 객체 - onegold */
-    SupportMapFragment mapFragment;
-    GoogleMap map;
-
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
     private DrawerLayout drawerLayout;
 
     @Override
@@ -79,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /* AutoPermission 권한 요청 */
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
     }
 
     @Override
@@ -91,36 +84,19 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* Google Map 처음 설정 */
-    private void initGoogleMap(){
-        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_monitor);
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                Log.d("Map", "지도 준비됨");
-                map = googleMap;
-            }
-        });
-        try{
-            MapsInitializer.initialize(this);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this,requestCode,permissions,this);
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
 
     }
-    /* Google Map 위치 정보 get, 위치 지정 */
-    private void startLocationService(){
-        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-        try{
-            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if(location != null){
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
+    @Override
+    public void onGranted(int i, String[] strings) {
 
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
