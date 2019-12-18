498b4ad8210c3673c6d6d1c52e782014b76b2f45
Onegold11 <43362141+Onegold11@users.noreply.github.com>
2019-12-18 15:35:44 +0900

    delete adapter package

--- app/src/main/java/com/guardiansofgalakddy/lvlmonitor/Onegold/adapter/AdapterState.java
+++
@@ -1,8 +0,0 @@
-package com.guardiansofgalakddy.lvlmonitor.Onegold.adapter;
-
-import com.guardiansofgalakddy.lvlmonitor.Onegold.adapter.RecycleAdapter;
-
-public interface AdapterState {
-    public void changeMonitor(RecycleAdapter adapter);
-    public void changeCollector(RecycleAdapter adapter);
-}
--- app/src/main/java/com/guardiansofgalakddy/lvlmonitor/Onegold/adapter/Collector.java
+++
@@ -1,4 +0,0 @@
-package com.guardiansofgalakddy.lvlmonitor.Onegold.adapter;
-
-public class Collector {
-}
